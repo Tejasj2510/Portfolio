3d8a51e42c19635ddf35d15bab38ed9facf01bbb
tejasj2510 <tejasjaulkarpatil@gmail.com>
2025-02-16 01:18:05 +0530

    tejas

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Tejasj2510/",
-  resume:"https://drive.google.com/file/d/1zKKMpGXAtkW61_c8qHzXs-cHsZYKm-_j/view?usp=sharing",
+  resume:"https://drive.google.com/file/d/1yTuIJwmufa2oNhOHutubRXjoP6QOdW0C/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/tejas-jaulkar-83820b335/",
   insta: "https://www.instagram.com/jaulkar_patil_3688",
   facebook: "https://www.facebook.com/tejas.jaulkar.5",
